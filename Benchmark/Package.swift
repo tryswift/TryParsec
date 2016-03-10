bb7ef369b17fad9583867fe4ff114bed336f747a
Norio Nomura <norio.nomura@gmail.com>
2016-03-10 16:53:44 +0900

    Remove `Benchmark` target from `Benchmark/Package.swift`

--- Benchmark/Package.swift
+++ Benchmark/Package.swift
@@ -2,9 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "TryParsecBenchmark",
-    targets: [
-        Target(name: "Benchmark"),
-    ],
     dependencies: [
         .Package(url: "..", majorVersion: 0),
         .Package(url: "https://github.com/thoughtbot/Curry.git", majorVersion: 2),
