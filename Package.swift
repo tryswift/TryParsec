af2f1449491fa76b1ae6d1e9924d80585c6e1c07
Yasuhiro Inami <inamiy@gmail.com>
2017-09-30 20:42:51 +0900

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "TryParsec",
     dependencies: [
-        .Package(url: "https://github.com/inamiy/Runes.git", "4.0.0-inamiy.1"),
+        .Package(url: "https://github.com/thoughtbot/Runes.git", majorVersion: 4),
         .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
     ]
 )
